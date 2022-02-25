b09e385b983df744a128b5441ac8e104e7710779
Mr.クローゼット <myuuto1012@icloud.com>
2022-02-25 16:47:41 +0900

    fsdss

--- shader/SpriteVs.hlsl
+++ shader/SpriteVs.hlsl
@@ -1,9 +1,11 @@
 #include "Sprite.hlsli"
+#include "Constants.hlsli"
 
 VS_OUT main(float4 position : POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD)
 {
     VS_OUT vout;
     vout.position = position;
+    //vout.position = mul(position, data);
     vout.color = color;
     vout.texcoord = texcoord;
     return vout;
