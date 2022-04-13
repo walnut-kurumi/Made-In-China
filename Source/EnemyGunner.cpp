7a67cfd0700129989eb88b108ceea42e3ea08161
soraoaros <taka.sora2236@icloud.com>
2022-04-13 11:51:10 +0900

    ^^

--- Source/EnemyGunner.cpp
+++ Source/EnemyGunner.cpp
@@ -16,7 +16,7 @@ EnemyGunner::EnemyGunner(ID3D11Device* device)
     const char* blow = "Data/Models/Enemy/Animations/GetHit1.fbx";
     const char* death = "Data/Models/Enemy/Animations/Death.fbx";
 
-    model = new Model(device, "Data/Models/Player/Jummo.fbx", true, 0);
+    model = new Model(device, "Data/Models/Player/Jummo.fbx");
 
     model->LoadAnimation(idle, 0, static_cast<int>(State::Idle));
     model->LoadAnimation(run, 0, static_cast<int>(State::Run));
