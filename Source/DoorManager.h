1d1cf7ac050a49eb6ef299c3d0b817704341fa46
soraoaros <taka.sora2236@icloud.com>
2022-05-12 15:33:44 +0900

    貫通床　ドア準備

--- Source/DoorManager.h
+++ Source/DoorManager.h
@@ -3,11 +3,6 @@
 #include "Door.h"
 #include "Collision.h"
 
-// コピーコンストラクタ防止マクロ
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-TypeName(const TypeName&); \
-void operator=(const TypeName&)
-
 class DoorManager
 {
 private:
@@ -16,23 +11,12 @@ private:
 	static DoorManager* instance;
 
 public:
-	// クラスのprivate:に置くことでデフォルトのコピーコンストラクタ
-	// 代入演算子を生成させないマクロ Readable codeから。
-	DISALLOW_COPY_AND_ASSIGN(DoorManager);
-
-	static DoorManager& Instance() {
-		return *instance;
+	// 唯一のインスタンス取得
+	static DoorManager& Instance()
+	{
+		static DoorManager instance;
+		return instance;
 	}
-
-	static void Create() {
-		if (!instance) instance = new DoorManager;
-	}
-
-	static void Destory() {
-		delete instance;
-		instance = nullptr;
-	}
-
 	void Init();
 
 	void Update(float elapsedTime);
@@ -47,8 +31,8 @@ public:
 
 	bool RayCast(const DirectX::XMFLOAT3& start, const DirectX::XMFLOAT3& end, HitResult& hit);
 
-	int GetStageCount()const { return static_cast<int>(doors.size()); }
-	Door* GetStage(int index) { return doors.at(index); }
+	int GetDoorCount()const { return static_cast<int>(doors.size()); }
+	Door* GetDoor(int index) { return doors.at(index); }
 
 private:
 	std::vector<Door*> doors;
