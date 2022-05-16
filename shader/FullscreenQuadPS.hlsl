4e54f82a6b544a400f3a5b12b0f170cd8c071998
kurumi <hokutono-kenta928@docomo.ne.jp>
2022-05-16 09:29:33 +0900

    sampler警告消した

--- shader/FullscreenQuadPS.hlsl
+++ shader/FullscreenQuadPS.hlsl
@@ -1,12 +1,9 @@
 #include "FullscreenQuad.hlsli"
 
-#define POINT 0
-#define LINEAR 1
-#define ANISOTROPIC 2
-SamplerState samplerStates[3] : register(s0);
+SamplerState samplerStates : register(s0);
 Texture2D textureMap : register(t0);
 
 float4 main(VS_OUT pin) : SV_TARGET
 {
-    return textureMap.Sample(samplerStates[LINEAR], pin.texcoord);
+    return textureMap.Sample(samplerStates, pin.texcoord);
 }
