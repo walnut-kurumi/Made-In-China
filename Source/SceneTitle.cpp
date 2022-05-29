9ef4329f84bf7655c590f502f60d64f4c7eca11e
soraoaros <taka.sora2236@icloud.com>
2022-05-29 16:22:40 +0900

    a

--- Source/SceneTitle.cpp
+++ Source/SceneTitle.cpp
@@ -98,7 +98,7 @@ void SceneTitle::Update(float elapsedTime)
         if (Fade::Instance().GetFadeOutFinish())
         {
            //SceneManager::Instance().ChangeScene(new SceneLoading(new SceneTutorial));
-           SceneManager::Instance().ChangeScene(new SceneLoading(new SceneGameSt2));
+           SceneManager::Instance().ChangeScene(new SceneLoading(new SceneGameSt3));
         }
     }
     // ‚¨‚í‚é
